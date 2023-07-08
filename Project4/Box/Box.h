b89905a752ec3f451b08ce8a5e5f547f9c0ffc60
JiYoung LEE <94455716+lakedata@users.noreply.github.com>
2023-07-08 13:04:52 +0900

    Add files via upload

---
+++ Project4/Box/Box.h
@@ -0,0 +1,12 @@
+#ifndef BOX_H
+#define BOX_H
+class Box {
+	int width, height;
+	char fil;
+public:
+	Box(int w, int h);
+	void setFill(char f);
+	void setSize(int w, int h);
+	void draw();
+}
+#endif
